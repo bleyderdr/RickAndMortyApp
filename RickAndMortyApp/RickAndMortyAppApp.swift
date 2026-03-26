1f1f4d4e725dde0ca1640184f1e33d7768c2f0b3
Bladimir <bladimirsl123@gmail.com>
2026-03-26 18:13:30 -0500

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Bladimir Salinas on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
