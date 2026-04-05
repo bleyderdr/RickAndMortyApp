4c30c5dc3a34a4bd1e15e6bc703cfed91174c4a4
Bladimir <bladimirsl123@gmail.com>
2026-04-05 16:15:01 -0500

    Test de HomePageViewModel

---
+++ RickAndMortyApp/Features/Presentation/Home/ViewModel/HomePageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomePageViewModel.swift
+//  RickAndMortyApp
+//
+//  Created by Bladimir Salinas on 5/04/26.
+//
+
+import Foundation
---
+++ RickAndMortyAppTests/Features/Presentation/HomePage/viewModel/HomePageViewModelTest.swift
@@ -0,0 +1,8 @@
+//
+//  HomePageViewModelTest.swift
+//  RickAndMortyAppTests
+//
+//  Created by Bladimir Salinas on 5/04/26.
+//
+
+import Foundation
