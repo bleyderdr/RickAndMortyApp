3ac98cd25ab75a78c4c1caf7ec986b63b6566909
Bladimir <bladimirsl123@gmail.com>
2026-04-05 16:16:07 -0500

    Test de HomePageViewModel 2

--- RickAndMortyApp/Features/Presentation/Home/ViewModel/HomePageViewModel.swift
+++ RickAndMortyApp/Features/Presentation/Home/ViewModel/HomePageViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import Observation
+
+@Observable class HomePageViewModel {
+    private let useCase: CharacterUseCase
+    
+    var characterList: [CharacterBusinessModel] = []
+    
+    var viewError: AppError?
+    
+    var hasError: Bool = false
+    
+    var isLoading: Bool = false
+    
+    private var currentPage: Int = 1
+    
+    init(useCase: CharacterUseCase = DefaultCharacterUseCase()) {
+        self.useCase = useCase
+    }
+    
+    func loadCharacterList() async {
+        guard !isLoading else { return }
+        
+        isLoading = true
+        
+        do {
+            let response = try await useCase.getCharacterList(pageNumber: "\(currentPage)")
+            
+            await MainActor.run {
+                characterList += response.results
+                isLoading = false
+                currentPage += 1
+                isLoading = false
+            }
+        } catch  {
+            await MainActor.run {
+                isLoading = false
+                viewError = .unEspectedError
+                hasError = true
+            }
+        }
+    }
+}
--- RickAndMortyAppTests/Features/Presentation/HomePage/viewModel/HomePageViewModelTest.swift
+++ RickAndMortyAppTests/Features/Presentation/HomePage/viewModel/HomePageViewModelTest.swift
@@ -5,4 +5,55 @@
 //  Created by Bladimir Salinas on 5/04/26.
 //
 
-import Foundation
+import XCTest
+@testable import RickAndMortyApp
+
+//MARK: - Test
+
+@MainActor
+class HomePageViewModelTest: XCTestCase {
+    
+    var sut: HomePageViewModel?
+    var sutFailure: HomePageViewModel?
+    
+    override func setUp() {
+        super.setUp()
+        sut = HomePageViewModel(useCase: DefaultCharacterUseCase(repository: DefaultCharacterRepository(apiService: CharacterListFakeApiServiceSuccess())))
+        sutFailure = HomePageViewModel(useCase: DefaultCharacterUseCase(repository: DefaultCharacterRepository(apiService: CharacterListFakeApiServiceFailure())))
+    }
+    
+    override func tearDown() {
+        sut = nil
+        sutFailure = nil
+        super.tearDown()//MARK: -
+    }
+    
+}
+
+//MARK: - Success Test
+
+extension HomePageViewModelTest {
+    func testSuccessCase_loadCharacterList() async {
+        await sut?.loadCharacterList()
+        XCTAssertFalse(sut?.characterList.isEmpty ?? false)
+        XCTAssertTrue(sut?.characterList.first?.id == 21)
+        XCTAssertNil(sut?.viewError)
+        if let hasError = sut?.hasError {
+            XCTAssertFalse(hasError)
+        }
+    }
+}
+
+
+//MARK: - Failure Test
+
+extension HomePageViewModelTest {
+    func testFailureCase_loadCharacterList() async {
+        guard let sutFailure else { return }
+        await sutFailure.loadCharacterList()
+        XCTAssertTrue(sutFailure.characterList.isEmpty)
+        XCTAssertNotNil(sutFailure.viewError)
+        XCTAssertTrue(sutFailure.hasError)
+    }
+}
+
